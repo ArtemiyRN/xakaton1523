e75ed16936a57e9c2a18211f3ef89b31679638c9
Matvey <69459474+FlameTigerFlash@users.noreply.github.com>
2022-01-22 21:29:46 +0300

    Add files via upload

--- Input.py
+++ Input.py
@@ -40,13 +40,15 @@ def stop(app):
 
 
 def action(value):
+    print(0)
     for i in in_process:
         apps[i][1] += value / len(in_process)
         apps[i][2] = apps[i][1] / apps[i][0]
 
 
-kb = threading.Thread(target = listen_ms)
+kb = threading.Thread(target = listen_kb)
 ms = threading.Thread(target=listen_ms)
+
 
 kb.start()
 ms.start()
